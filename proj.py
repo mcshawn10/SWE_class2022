fd21538f69fd5aea4d5bc357ae36f88c0a16b2f5
Shawn <shawnmcneill10@gmail.com>
2022-03-24 21:29:44 -0500

    class

--- proj.py
+++ proj.py
@@ -2,29 +2,27 @@
 from collections import Counter
 
 
-txt_file = open('cat in the hat.txt', 'r')
-txt = txt_file.read()
-txt = txt.lower()
-all_words = txt.split()
-all_words = [word.strip(" '.,!?") for word in all_words]
-all_words.sort()
-counter = Counter(all_words)
-
-unq = set(all_words)
-
-
-for i in unq:
-
-    print(i, "appears" , counter[i])
-
 
 class StringStat:
     
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path
 
     def return_stats(self):
+        txt_file = open(self.path, 'r')
+        txt = txt_file.read()
+        txt = txt.lower()
+        all_words = txt.split()
+        all_words = [word.strip(" '.,!?") for word in all_words]
+        all_words.sort()
+        counter = Counter(all_words)
+        unq = set(all_words)
+
+        for i in unq:
+
+            print(i, "appears" , counter[i])
+
+    def replace_word(self):
         pass
-    
 
 
