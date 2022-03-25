35535aa8a7513db1ca8dc9cfe505d827e983c616
Shawn <shawnmcneill10@gmail.com>
2022-03-24 21:28:53 -0500

    class

--- proj.py
+++ proj.py
@@ -1,7 +1,30 @@
 
+from collections import Counter
 
-print("hello world")
-print("testing")
-print("final test")
-print("whats up")
+
+txt_file = open('cat in the hat.txt', 'r')
+txt = txt_file.read()
+txt = txt.lower()
+all_words = txt.split()
+all_words = [word.strip(" '.,!?") for word in all_words]
+all_words.sort()
+counter = Counter(all_words)
+
+unq = set(all_words)
+
+
+for i in unq:
+
+    print(i, "appears" , counter[i])
+
+
+class StringStat:
+    
+    def __init__(self):
+        pass
+
+    def return_stats(self):
+        pass
+    
+
 
